06045c12398822955b2591566a15a58740c2e5e5
Student <example@example.com>
2020-01-15 09:06:14 -0800

    fix toUpperCase on nameInput variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
-    $("#madlib").submit(function(event) {
-      var person1Input = $("input#person1").val();
-      var place1Input = $("input#place1").val();
-      var noun1Input = $("input#noun1").val();
-      var verb1Input = $("input#verb1").val();
-     
-    console.log("made it through submit");
-    
-      $(".person1").append(person1Input);
-      $(".place1").append(place1Input);
-      $(".noun1").append(noun1Input);
-      $(".verb1").append(verb1Input);
+    $("#name-form").submit(function(event) {
+      var nameInput = $("input#name").val().toUpperCase(); 
 
   
-      $("#story").show();
+      
+      $(".name").append(nameInput);
+
+      $("#letter").show();
   
       event.preventDefault();
     });
